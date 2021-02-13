bec4f7eda9bf42d6327bd93dff259a42b205ca85
Awais Mushtaq <ovaiz.mushtaq@gmail.com>
2021-02-13 04:08:22 +0100

    starship API endpoint

--- app/api.py
+++ app/api.py
@@ -11,7 +11,23 @@ from app.input_schema import TrainingAPISchema, LoginInputSchema
 swagger = Swagger(app, template=app.config['SWAGGER_TEMPLATE'])
 
 
-@app.route('/login', methods=['POST'])
-def login():
-
+@app.route('/starships', methods=['POST'])
+def starships():
+    """Endpoint for training dss system for munitions
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: sort
+        in: formData
+        type: integer
+        enum: [0,1,2]
+        required: true
+        default: 0
+        description: Microservice to return starships from the Star Wars movies, sorted by the hyperdrive rating. 0 without sort, 1 sort Asc, 2, sort Desc
+    responses:
+      200:
+        description: JSON object containing starships
+        examples:
+          rgb: []
+    """
     return 0
